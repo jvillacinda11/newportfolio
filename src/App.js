8634a89151c215832598ac45be42c51df4a496cb
jvilla <josevillacinda96@gmail.com>
2021-07-31 21:33:46 -0700

    initial react app created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<>
+<h1>This is my new react portfolio</h1>
+<p>It's been a while lmao</p>
+</>
+  );
+}
+
+export default App;
